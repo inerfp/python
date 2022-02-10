4c274e534603b3a73863da4dbc6d70debbacf949
brocar <97797948+inerfp@users.noreply.github.com>
2022-02-11 00:21:36 +0900

    Update book.py

--- book.py
+++ book.py
@@ -3,10 +3,14 @@
 # 회원 여부
 
 # 대여 책, 책 대여 기간, 대여 여부
-
 # 렌탈 조건 - 대여할 책, 대여 기간
 # 반납 조건 - 대여한 책, 대여 기간
 import csv
+import os
+from pydoc import replace
+
+file_path = '/Users/nerda/Documents/side_project/python/books/template/'
+
 
 class MemberJoin:
     def join(self):
@@ -14,13 +18,11 @@ class MemberJoin:
         mnumber = int(input("회원 번호를 받으세요 : "))
         join_name = input("이름을 입력하세요 : ")
         join_age = int(input("나이를 입력하세요 : "))
-        with open('members.csv', 'a', newline='') as csvfile:
+        with open('{}members.csv'.format(file_path), 'a', newline='') as csvfile:
             fieldnames = ['id', 'name', 'age']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            # writer.writeheader()
             writer.writerow(
                 {'id': mnumber, 'name': join_name, 'age': join_age})
-        # member = {"id": mnumber, "name": join_name, "age": join_age}
 # 입력 정보 / 회원 정보 / 이름 / 나이 / 빌린 책 정보 (bookid)
 
 
@@ -29,35 +31,31 @@ class BookList:
         print("책 추가 \n")
         bnumber = int(input("책 번호를 받으세요 : "))
         book_name = input("책 이름을 입력하세요 : ")
-        with open('books.csv', 'a', newline='') as csvfile:
-            fieldnames = ['id', 'bookname', 'have', 'rantal']
+        with open('{}books.csv'.format(file_path), 'a', newline='') as csvfile:
+            fieldnames = ['id', 'bookname', 'have']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             # writer.writeheader()
             writer.writerow(
-                {'id': bnumber, 'bookname': book_name, 'have': True, 'rantal': True})
+                {'id': bnumber, 'bookname': book_name, 'have': True})
 # 입력 정보 / 책 정보 / 책 이름
 
 
 class Book_Rantal:
     def book_rantal(self):
-        print("회원 정보 입력\n")
+        print("빌릴 책 입력\n")
+        with open('/Users/nerda/Documents/side_project/python/books/template/rantal.csv', 'a', newline='') as csvfile:
+            fieldnames = ['id', 'memberid', 'bookid']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow('id': None, 'memberid': None, 'bookid': None)
 
 
-# class Book_return(Book):
-#     def book_return(self, bookname, period):
-#         self.book_name = bookname
-#         self.book_period = period
-
-
-# class Book_booking(Book):
-#     def book_booking(self, bookname, period, exist):
 # 파일 입출력
 members = []
 books = []
 while True:
     print("1. 회원가입")
     print("2. 도서 정보 입력")
-    print("3. 도서 대여")
+    print("3. 회원 이름 변경")
     print("4. 도서 반납")
     a = int(input("숫자를 입력하시오 \n"))
 
@@ -72,30 +70,13 @@ while True:
     # 회원 정보 입력 체크 맴버 db 접근 (csv open) > 회원 정보 리스트 넣기 > 입력값 회원 여부 체크
     # 빌릴 책 입력 > 보유 책 체크 > 대여 상태 체크
     if a == 3:
-        with open('members.csv', newline='', encoding='utf-8') as csvfile:
-            memberreader = csv.reader(csvfile)
-            a = input("가입한 이름을 입력하세요")
-            for row in memberreader:
-                print(row)
+        with open('{}members.csv'.format(file_path), 'w', encoding='utf-8') as csvfile:
+            a = input("회원 이름 입력 ")
+            b = input("변경할 이름 입력")
+            members = csv.writer(csvfile)
+            for row in members:
                 if a == row[1]:
-                    print("회원 정보가 일치")
-                    compare_rantal = Book_Rantal()
-                    cr = compare_rantal()
-                else:
-                    print("회원 정보가 불일치")
-                
-
-        # if a == 2:
-        #     number = int(input("변경하고자 하는 회원 번호를 말씀해주세요"))
-        #     change_members = []
-        #     for i in members:
-        #         if i["id"] == number:
-        #             change_members = members[i]
-        #             change_name = input("변경할 이름을 입력하세요 ")
-        #             change_age = int(input("변경할 나이를 입력하세요 "))
-        #             change_members["name"] = change_name
-        #             change_members["age"] = change_age
-        #         print(i)
+                    members.writerows([b])
 
     if a == 0:
         print(members)
